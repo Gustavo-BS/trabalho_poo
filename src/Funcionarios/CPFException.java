60600e16dba5f1064513955be036e629cb9c3091
Gustavo Borges <gustavobprog@gmail.com>
2023-11-16 11:23:32 -0300

    adicionado validador cpf

---
+++ src/Funcionarios/CPFException.java
@@ -0,0 +1,7 @@
+package Funcionarios;
+
+public class CPFException extends IllegalArgumentException{
+    public CPFException(String msg){
+        super(msg);
+    }
+}
--- src/Funcionarios/Funcionario.java
+++ src/Funcionarios/Funcionario.java
@@ -1,5 +1,7 @@
 package Funcionarios;
 
+import java.util.regex.Pattern;
+
 public abstract class Funcionario {
     private String nome;
     private String CPF;
@@ -10,9 +12,41 @@ public abstract class Funcionario {
 
     public Funcionario(String nome, String CPF, String RG, EstadoCivil estadoCivil, String endereco) {
         this.nome = nome;
-        this.CPF = CPF;
         this.RG = RG;
         this.estadoCivil = estadoCivil;
         this.endereco = endereco;
+        validarCPF(CPF);
+    }
+
+    private void validarCPF(String CPF) {
+        if (!CPFValido(CPF)) {
+            throw new CPFException("CPF INVALIDO: " + CPF);
+        }
+        this.CPF = CPF;
+    }
+    private boolean CPFValido(String CPF) {
+        if (!Pattern.matches("\\d{11}", CPF)) {
+            return false;
+        }
+        int[] numeros = new int[11];
+        for (int i = 0; i < 11; i++) {
+            numeros[i] = Character.getNumericValue(CPF.charAt(i));
+        }
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += numeros[i] * (10 - i);
+        }
+        int resto = soma % 11;
+        int digitoEsperado1 = (resto < 2) ? 0 : (11 - resto);
+        if (numeros[9] != digitoEsperado1) {
+            return false;
+        }
+        soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += numeros[i] * (11 - i);
+        }
+        resto = soma % 11;
+        int digitoEsperado2 = (resto < 2) ? 0 : (11 - resto);
+        return numeros[10] == digitoEsperado2;
     }
 }
