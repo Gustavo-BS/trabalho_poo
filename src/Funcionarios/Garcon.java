e47851f5aaafe952d6e881482d66d41a8c923ca9
poderossisímo Casão <100032236+supercasao@users.noreply.github.com>
2023-12-06 11:06:37 -0300

    Update Garcon.java

--- src/Funcionarios/Garcon.java
+++ src/Funcionarios/Garcon.java
@@ -3,7 +3,9 @@ package Funcionarios;
 import java.io.Serializable;
 import java.util.Date;
 
-public class Garcon extends Funcionario implements Serializable{
+import java.io.Serializable;
+
+public class Garcon extends Funcionario implements Serializable {
     private static final long serialVersionUID = 1L;
     private Date admissao;
     private String carteiraTrabalho;
@@ -11,24 +13,18 @@ public class Garcon extends Funcionario implements Serializable{
     private DiaFolga diaFolga;
     private int numeroPedidos;
 
-    public Garcon(String nome, String CPF){
-        super(nome,CPF);
-        // this.admissao = admissao;
-        // this.carteiraTrabalho = carteiraTrabalho;
-        // this.salarioBase = salarioBase;
-        // this.diaFolga = diaFolga;
-    }
-
-    public void adicionarPedidoRealizado() {
-        this.numeroPedidos++;
+    public Garcon(String nome, String CPF) {
+        super(nome, CPF, estadocivil);
+        this.admissao = null; // Valor inicial para admissao
+        this.carteiraTrabalho = ""; // Valor inicial para carteiraTrabalho
+        this.salarioBase = 0.0; // Valor inicial para salarioBase
+        this.diaFolga = null; // Valor inicial para diaFolga
+        this.numeroPedidos = 0; // Valor inicial para numeroPedidos
     }
 
     @Override
-    public double calcularSalario() {
-        double salarioTotal = getSalario();
-        if (numeroPedidos > 100) {
-            salarioTotal += 100; // Bônus fixo se o número de pedidos for maior que 100
-        }
-        return salarioTotal;
+   public double calcularSalario(double totalVendas, double salarioBase) {
+        double comissao = totalVendas * 0.10;
+        return salarioBase + comissao;
     }
 }
