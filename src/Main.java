266182808809b3e5676fe914fbfaa725671652a1
poderossisímo Casão <100032236+supercasao@users.noreply.github.com>
2023-12-06 11:29:28 -0300

    Update Main.java
    finallll

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-
-
-
 import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -12,133 +9,107 @@ import Funcionarios.Cozinheiro;
 import Funcionarios.Funcionario;
 import Pedidos.Pedido;
 
-public class Main {
-    private static List<Pedido> pedidos = new ArrayList<>();
-    private static List<Funcionario> funcionarios = new ArrayList<>();
+public class Main{
+    private static final String PRATOS_FILE = "pratos.ser";
+    private static final String FUNCIONARIOS_FILE = "funcionarios.ser";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Prato> pratos = carregarPratos();
+        List<Funcionario> funcionarios = carregarFuncionarios();
 
         int opcao;
         do {
-            System.out.println("== Sistema de Restaurante ==");
-            System.out.println("1. Realizar pedido");
-            System.out.println("2. Adicionar funcionário");
-            System.out.println("3. Salvar dados");
-            System.out.println("4. Carregar dados");
-            System.out.println("0. Sair");
+            exibirMenu();
             System.out.print("Escolha uma opção: ");
             opcao = scanner.nextInt();
-            scanner.nextLine(); // Limpar o buffer
+            scanner.nextLine(); // Consumir a quebra de linha
 
             switch (opcao) {
                 case 1:
-                    realizarPedido(scanner);
+                    cadastrarPrato(scanner, pratos);
                     break;
                 case 2:
-                    adicionarFuncionario(scanner);
+                    cadastrarFuncionario(scanner, funcionarios);
                     break;
                 case 3:
-                    salvarDados();
+                    exibirPratos(pratos);
                     break;
                 case 4:
-                    carregarDados();
+                    exibirFuncionarios(funcionarios);
+                    break;
+                case 5:
+                    salvarPratos(pratos);
+                    salvarFuncionarios(funcionarios);
                     break;
                 case 0:
-                    System.out.println("Saindo do sistema...");
+                    System.out.println("Saindo...");
                     break;
                 default:
-                    System.out.println("Opção inválida! Tente novamente.");
+                    System.out.println("Opção inválida!");
             }
         } while (opcao != 0);
 
         scanner.close();
     }
-    private static void salvarDados() {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("dados.bin"))) {
-            out.writeObject(pedidos);
-            out.writeObject(funcionarios);
-            System.out.println("Dados salvos com sucesso!");
+
+    private static void exibirMenu() {
+        System.out.println("\n--- Restaurante App ---");
+        System.out.println("1. Cadastrar Prato");
+        System.out.println("2. Cadastrar Funcionário");
+        System.out.println("3. Exibir Pratos");
+        System.out.println("4. Exibir Funcionários");
+        System.out.println("5. Salvar Dados");
+        System.out.println("0. Sair");
+    }
+
+    private static void cadastrarPrato(Scanner scanner, List<Prato> pratos) {
+        // Implemente a lógica para cadastrar um prato
+    }
+
+    private static void cadastrarFuncionario(Scanner scanner, List<Funcionario> funcionarios) {
+        // Implemente a lógica para cadastrar um funcionário
+    }
+
+    private static void exibirPratos(List<Prato> pratos) {
+        // Implemente a lógica para exibir os pratos cadastrados
+    }
+
+    private static void exibirFuncionarios(List<Funcionario> funcionarios) {
+        // Implemente a lógica para exibir os funcionários cadastrados
+    }
+
+    private static List<Prato> carregarPratos() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(PRATOS_FILE))) {
+            return (List<Prato>) ois.readObject();
+        } catch (IOException | ClassNotFoundException | ClassCastException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    private static List<Funcionario> carregarFuncionarios() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FUNCIONARIOS_FILE))) {
+            return (List<Funcionario>) ois.readObject();
+        } catch (IOException | ClassNotFoundException | ClassCastException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    private static void salvarPratos(List<Prato> pratos) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(PRATOS_FILE))) {
+            oos.writeObject(pratos);
+            System.out.println("Pratos salvos com sucesso!");
         } catch (IOException e) {
-            System.out.println("Erro ao salvar dados: " + e.getMessage());
+            System.out.println("Erro ao salvar os pratos: " + e.getMessage());
         }
     }
 
-    private static void carregarDados() {
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("dados.bin"))) {
-            pedidos = (List<Pedido>) in.readObject();
-            funcionarios = (List<Funcionario>) in.readObject();
-            System.out.println("Dados carregados com sucesso!");
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Erro ao carregar dados: " + e.getMessage());
+    private static void salvarFuncionarios(List<Funcionario> funcionarios) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FUNCIONARIOS_FILE))) {
+            oos.writeObject(funcionarios);
+            System.out.println("Funcionários salvos com sucesso!");
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar os funcionários: " + e.getMessage());
         }
-    }
-
-    private static void realizarPedido(Scanner scanner) {
-        System.out.println("== Realizar Pedido ==");
-        System.out.println("Selecione o tipo de funcionário que preparou o pedido:");
-        System.out.println("1. Cozinheiro");
-        System.out.println("2. Outro");
-        int opcaoFuncionario = scanner.nextInt();
-        scanner.nextLine(); // Limpar o buffer
-
-        Funcionario funcionario;
-        if (opcaoFuncionario == 1) {
-            funcionario = criarCozinheiro(scanner);
-        } else {
-            funcionario = criarFuncionario(scanner);
-        }
-
-        System.out.println("Digite a data do pedido (formato: yyyy-MM-dd):");
-        LocalDate dataPedido = LocalDate.parse(scanner.nextLine());
-
-        System.out.println("Digite o horário do pedido (formato: HH:mm:ss):");
-        LocalTime horarioPedido = LocalTime.parse(scanner.nextLine());
-
-        Pedido pedido = new Pedido(funcionario, dataPedido, horarioPedido);
-        pedidos.add(pedido);
-
-        System.out.println("Pedido realizado com sucesso!");
-    }
-
-    private static Cozinheiro criarCozinheiro(Scanner scanner) {
-        // Lógica para criar um cozinheiro
-        System.out.println("Digite o nome do cozinheiro:");
-        String nome = scanner.nextLine();
-
-        System.out.println("Digite o CPF do cozinheiro:");
-        String cpf = scanner.nextLine();
-
-        return new Cozinheiro(nome, cpf);
-    }
-
-    private static Funcionario criarFuncionario(Scanner scanner) {
-        // Lógica para criar um funcionário genérico
-        System.out.println("Digite o nome do funcionário:");
-        String nome = scanner.nextLine();
-
-        System.out.println("Digite o CPF do funcionário:");
-        String cpf = scanner.nextLine();
-
-        return new Funcionario(nome, cpf);
-    }
-
-    private static void adicionarFuncionario(Scanner scanner) {
-        System.out.println("== Adicionar Funcionário ==");
-        System.out.println("Selecione o tipo de funcionário a ser adicionado:");
-        System.out.println("1. Cozinheiro");
-        System.out.println("2. Outro");
-        int opcaoFuncionario = scanner.nextInt();
-        scanner.nextLine(); // Limpar o buffer
-
-        Funcionario novoFuncionario;
-        if (opcaoFuncionario == 1) {
-            novoFuncionario = criarCozinheiro(scanner);
-        } else {
-            novoFuncionario = criarFuncionario(scanner);
-        }
-
-        funcionarios.add(novoFuncionario);
-        System.out.println("Funcionário adicionado com sucesso!");
     }
 }
